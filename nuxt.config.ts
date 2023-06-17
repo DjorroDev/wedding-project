7c95a679a5b44debf25b5224648b2cc2bbff7767
Djorro <DjorroDev@users.noreply.github.com>
2023-06-18 01:41:22 +0700

    add baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-swiper", "@nuxt/image-edge", "nuxt-icon", "nuxt-headlessui"],
+
   runtimeConfig: {
+    baseURL: process.env.NUXT_APP_BASE_URL,
     public: {
       apiKey: process.env.NUXT_FB_API_KEY,
       authDomain: process.env.NUXT_FB_AUTH_DOMAIN,
@@ -9,12 +12,29 @@ export default defineNuxtConfig({
       messagingSenderId: process.env.NUXT_FB_MESSAGING_SENDER_ID,
       appId: process.env.NUXT_FB_APP_ID,
       measurementId: process.env.NUXT_FB_MEASUREMENT_ID,
+      motion: {
+        directives: {
+          custom: {
+            initial: {
+              opacity: 0,
+              y: 100,
+            },
+            visibleOnce: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                type: "spring",
+                stiffness: "30",
+                mass: "1",
+                delay: 200,
+              },
+            },
+          },
+        },
+      },
     },
   },
-  modules: ["nuxt-swiper", "@nuxt/image-edge", "nuxt-icon", "nuxt-headlessui"],
-  // plugins: [
-  //     'firestore'
-  // ],
+
   swiper: {},
   image: {
     provider: "storyblok",
